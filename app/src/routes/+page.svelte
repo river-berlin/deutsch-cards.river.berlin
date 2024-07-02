69660dc7a31ad0e71450353446627972323b4cc6
Aditya Shankar <aditniru@gmail.com>
2024-07-02 22:44:24 +0000

    setup basic application and tailwindcss

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<progress class="progress progress-secondary w-56" value="70" max="100"></progress>
+
+<div class="card bg-primary text-primary-content w-96">
+    <div class="card-body">
+      <h2 class="card-title">Card title!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn">Buy Now</button>
+      </div>
+    </div>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
